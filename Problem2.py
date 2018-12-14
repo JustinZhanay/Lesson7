ad7e4ec4970cd9978052477c42477683e8d9188f
JustinZhanay <43792367+JustinZhanay@users.noreply.github.com>
2018-12-14 09:29:43 -0500

    Add files via upload

---
+++ Problem2.py
@@ -0,0 +1,8 @@
+print("-"*65)
+print('Welcome to the mulitplier-inator: by Justin Siguachi')
+x=input('Pick a number: ')
+int(x)
+y=input('Pick another number:')
+int(y)
+product= 'x' * 'y'
+print(product)
